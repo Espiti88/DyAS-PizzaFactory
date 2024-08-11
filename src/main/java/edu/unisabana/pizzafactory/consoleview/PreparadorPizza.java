2ddbc1e490fec8899993f1149148c25665415f99
Samuel Espitia <samuelespitia88@gmail.com>
2024-08-11 11:50:58 -0500

    Seleccion de pizzas predeterminadas

--- src/main/java/edu/unisabana/pizzafactory/consoleview/PreparadorPizza.java
+++ src/main/java/edu/unisabana/pizzafactory/consoleview/PreparadorPizza.java
@@ -3,89 +3,48 @@ package edu.unisabana.pizzafactory.consoleview;
 import edu.unisabana.pizzafactory.model.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class PreparadorPizza {
 
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int opcion = 0;
-        Informacion info = null;
 
-        while(true){
-            System.out.println("¡Bienvenido! Elige el tipo de masa de tu preferencia:");
-            System.out.println("1) Delgada\n2) Gruesa\n3) Integral\n4) Salir");
-            opcion = sc.nextInt();
-            switch (opcion){
-                case 1: {
-                    info = GenerarPizza();
-                    PizzaDelgada delgada = new PizzaDelgada(info.getTamano(), info.getIngredientes());
-                    delgada.aplicarIngredientes();
-                    delgada.crearAmasador();
-                    delgada.crearMoldeador();
-                    delgada.crearHorneador();
-                    break;
-                }case 2: {
-                    info = GenerarPizza();
-                    PizzaGruesa gruesa = new PizzaGruesa(info.getTamano(), info.getIngredientes());
-                    gruesa.aplicarIngredientes();
-                    gruesa.crearAmasador();
-                    gruesa.crearMoldeador();
-                    gruesa.crearHorneador();
-                    break;
-                }case 3: {
-                    info = GenerarPizza();
-                    PizzaIntegral integral = new PizzaIntegral(info.getTamano(), info.getIngredientes());
-                    integral.aplicarIngredientes();
-                    integral.crearAmasador();
-                    integral.crearMoldeador();
-                    integral.crearHorneador();
-                    break;
-                }case 4: {
-                    break;
-                }
-                default: {
-                    System.out.println("¡Elegiste un tipo inválido!\n");
-                }
-            }
-        }
-    }
+        //Pizza mediana gruesa
+        ArrayList<Ingrediente> ingredientesGruesaMediana = new ArrayList<>(Arrays.asList(
+                new Ingrediente("Queso", 1),
+                new Ingrediente("Jamon", 2)
+        ));
+        PizzaGruesa gruesa = new PizzaGruesa("MEDIANA", ingredientesGruesaMediana);
+        gruesa.aplicarIngredientes();
+        gruesa.crearAmasador();
+        gruesa.crearMoldeador();
+        gruesa.crearHorneador();
 
-    public static Informacion GenerarPizza(){
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Ingrediente> ingredientes = new ArrayList<>();
-        ArrayList<Object> informacion = new ArrayList<>();
+        //Pizza pequeña delgada
+        ArrayList<Ingrediente> ingredientesDelgadaPequena = new ArrayList<>(Arrays.asList(
+                new Ingrediente("Tomate", 1),
+                new Ingrediente("Peperoni", 2),
+                new Ingrediente("Cebolla", 1)
+        ));
+        PizzaDelgada delgada = new PizzaDelgada("PEQUEÑA", ingredientesDelgadaPequena);
+        delgada.aplicarIngredientes();
+        delgada.crearAmasador();
+        delgada.crearMoldeador();
+        delgada.crearHorneador();
 
-        String nombre = "";
-        int cantidad = -1;
-        String tamano = "";
-        while(tamano.equals("")){
-            System.out.println("Ingresa el tamaño (pequeña o mediana) de tu nueva pizza: ");
-            tamano = sc.nextLine();
-            sc.nextLine();
-            if(tamano.toUpperCase().equals("PEQUEÑA") ||tamano.toUpperCase().equals("MEDIANA")){
-                while(!nombre.toUpperCase().equals("SALIR")){
-                    System.out.println("Ingresa el ingrediente que deseas (Escribe 'salir' para salir): ");
-                    nombre = sc.nextLine();
-                    if(!nombre.toUpperCase().equals("SALIR")){
-                        System.out.println(nombre);
-                        System.out.println("¿Cuántas unidades quieres de ese ingrediente?: ");
-                        cantidad = sc.nextInt();
-                        sc.nextLine();
-                        if(cantidad > 0){
-                            ingredientes.add(new Ingrediente(nombre, cantidad));
-                        }else{
-                            System.out.println("¡Ingresaste una cantidad inválida!\n");
-                        }
-                    }
-                }
-                Informacion info =  new Informacion(tamano.toUpperCase(), ingredientes);
-                return info;
-            }else{
-                System.out.println("¡Ingresaste un tamaño inválido!\n");
-            }
-        }
-        return null;
+        //Pizza mediana integral
+        ArrayList<Ingrediente> ingredientesIntegralMediana = new ArrayList<>(Arrays.asList(
+                new Ingrediente("Lechuga", 1),
+                new Ingrediente("Quinua", 2)
+        ));
+        PizzaIntegral integral = new PizzaIntegral("MEDIANA", ingredientesIntegralMediana);
+        integral.aplicarIngredientes();
+        integral.crearAmasador();
+        integral.crearMoldeador();
+        integral.crearHorneador();
     }
 }
